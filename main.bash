abac423a163fb886698f4b7ab4b49417652f5103
Umar Khorami <umar.khorami@gmail.com>
2023-12-20 00:34:24 +0000

    first commit!

--- main.bash
+++ main.bash
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Define the HTML segment to be duplicated
 html_segment=$(cat <<'EOF'
 <p id="green"><a href="javascritp.pdf">Learn JavaScript Course</a></p>
 EOF
 )
 
-# Use sed to append the HTML segment two more times after 
-# the line containing the original segment in index.html
 sed -i "/<p id=\"green\"><a href=\"javascritp.pdf\">Learn JavaScript Course<\/a><\/p>/ {
   a $html_segment
   a $html_segment
